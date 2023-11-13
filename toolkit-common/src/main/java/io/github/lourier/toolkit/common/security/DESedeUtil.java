d24a269abd2c460256f0f06ae45241488a8cc1dc
LourierL <liuhlei1992@gmail.com>
2023-11-13 16:21:28 +0800

    fix DESede algorithm

--- toolkit-common/src/main/java/io/github/lourier/toolkit/common/security/DESedeUtil.java
+++ toolkit-common/src/main/java/io/github/lourier/toolkit/common/security/DESedeUtil.java
@@ -2,6 +2,7 @@ package io.github.lourier.toolkit.common.security;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -9,43 +10,52 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESedeKeySpec;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.Security;
+import java.security.spec.InvalidKeySpecException;
 
 /**
  * @Description: DESede 算法实现
- * JDK 支持DESede算法实现，支持112位或168位密钥长度；通过Bouncy Castle可将DESede算法密钥延长至128位或192位
  * @Date: 2023/11/10 11:26
  * @Author: Lourier
  */
 public class DESedeUtil {
 
+    /**
+     * 密钥算法
+     * JDK 支持DESede算法实现，支持112位或168位密钥长度；
+     * 通过 Bouncy Castle 可将算法密钥延长至128位或192位
+     * */
+    public static final String KEY_ALGORITHM = "DESede";
+
+    /**
+     * 加/解密算法 / 工作模式 / 填充方式；
+     * JDK 支持 PKCS5Padding 填充方式。Bouncy Castle 支持 PKCS7Padding 填充方式
+     * */
+    public static final String CIPHER_ALGORITHM = "DESede/ECB/PKCS7Padding";
+
+    static {
+        Security.addProvider(new BouncyCastleProvider()); // 导入 Bouncy Castle 库实现
+    }
+
     public static byte[] encrypt(byte[] data, byte[] key) {
         return doFinal(data, key, Cipher.ENCRYPT_MODE);
     }
 
-    public static String encrypt2HexString(byte[] data, byte[] key) {
-        byte[] bytes = encrypt(data, key);
-        return Hex.encodeHexString(bytes);
-    }
 
     public static byte[] decrypt(byte[] data, byte[] key) {
         return doFinal(data, key, Cipher.DECRYPT_MODE);
     }
 
-    public static String decrypt2HexString(byte[] data, byte[] key) {
-        byte[] decrypt = decrypt(data, key);
-        return Hex.encodeHexString(decrypt);
-    }
-
     public static byte[] initKey() {
         try {
-            KeyGenerator keyGenerator = KeyGenerator.getInstance("DESede", "BC"); // 表示 Bouncy Castle 提供
-            keyGenerator.init(192); // 指定密钥长度
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(KEY_ALGORITHM, "BC"); // 表示 Bouncy Castle 提供
+            keyGenerator.init(192); // 指定密钥长度，可以是 112、168、192，这里选择最长的密钥长度
             SecretKey secretKey = keyGenerator.generateKey();
             return secretKey.getEncoded();
         } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
@@ -64,13 +74,19 @@ public class DESedeUtil {
     }
 
     private static Key toKey(byte[] key) {
-        return new SecretKeySpec(key, "DESede");
+        try {
+            DESedeKeySpec keySpec = new DESedeKeySpec(key);
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(KEY_ALGORITHM);
+            return keyFactory.generateSecret(keySpec);
+        } catch (InvalidKeyException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static byte[] doFinal(byte[] data, byte[] key, int mode) {
         Key k = toKey(key);
         try {
-            Cipher cipher = Cipher.getInstance("DESede/ECB/PKCS7Padding", "BC"); // 表示 Bouncy Castle 提供
+            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM, "BC"); // 表示 Bouncy Castle 提供
             cipher.init(mode, k);
             return cipher.doFinal(data);
         } catch (NoSuchAlgorithmException | NoSuchProviderException | NoSuchPaddingException |
---
+++ toolkit-common/src/test/java/io/github/lourier/toolkit/common/security/DESedeUtilTest.java
@@ -0,0 +1,30 @@
+package io.github.lourier.toolkit.common.security;
+
+import org.apache.commons.codec.binary.Base64;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Date: 2023/11/13 15:36
+ * @Author: Lourier
+ */
+public class DESedeUtilTest {
+
+    @Test
+    public void testInitKey() {
+        byte[] key = DESedeUtil.initKey();
+        Assertions.assertEquals(24, key.length); // 192bit -> 24byte
+        String hexKey = DESedeUtil.hexStringKey();
+        Assertions.assertEquals(48, hexKey.length());
+    }
+
+    @Test
+    public void testEncryptAndDecrypt() {
+        byte[] key = DESedeUtil.initKey();
+        String input = "DESede";
+        byte[] encrypt = DESedeUtil.encrypt(input.getBytes(), key);
+        byte[] decrypt = DESedeUtil.decrypt(encrypt, key);
+        Assertions.assertEquals(input, new String(decrypt));
+    }
+
+}
